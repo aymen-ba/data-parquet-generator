2c7ffcd1775582b23144a483cf6efd6ccf2c2cf0
aymen-ba <ibnali.aymen@gmail.com>
2019-12-03 16:41:13 +0100

    initial commit

---
+++ build.sbt
@@ -0,0 +1,9 @@
+name := "Parquet4S-v2"
+
+version := "0.2"
+
+scalaVersion := "2.12.0"
+
+libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.6.5"
+
+libraryDependencies += "com.github.mjakubowski84" % "parquet4s-core_2.12" % "0.9.1"
---
+++ src/main/scala/threading.scala
@@ -0,0 +1,95 @@
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.{Calendar, Random}
+
+import com.github.mjakubowski84.parquet4s.{ParquetReader, ParquetWriter}
+
+import scala.collection.mutable.ArrayBuffer
+
+
+// transaction model
+case class Transaction(timestamp: String, idProduit: String, idTransaction: String, idMagasin: String, quantite: String)
+
+
+class MyThread(startIdx: Int, rd: Random, idsMagasin: Array[String]) extends Thread {
+
+
+  override def run()
+  {
+    val timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss.sssZ").format(Calendar.getInstance.getTime)
+
+    val path = "hdfs://127.0.0.1:/tmp/v2/part_"+ this.startIdx +".parquet"
+    println(path)
+
+    // Displaying the thread that is running
+    println("Thread " + Thread.currentThread().getName() + " is running.")
+
+
+    // generate transactions
+    var transactions = ArrayBuffer[Transaction]()
+
+    for (j <- this.startIdx*100000 until 1000000*(this.startIdx+1)){
+      //write transaction: timeStamp|idProduit|idTransaction|idMagasin|qte
+
+      val repeatTransaction = rd.nextInt(9) + 1
+      val indexMagasin = rd.nextInt(1200)
+
+
+      for( r <- 0 until repeatTransaction){
+        val productId = rd.nextInt(10000) + 1
+        val qte = rd.nextInt(9) + 1
+
+        val transaction = Transaction(timeStamp,productId.toString,(j + 1).toString,this.idsMagasin(indexMagasin),qte.toString)
+
+        try {
+          transactions += transaction
+
+        } catch {
+          case ex: IOException => {
+            ex.printStackTrace()
+          }
+        }
+      }
+    }
+
+
+    // writing
+    ParquetWriter.write(path, transactions.toStream)
+  }
+}
+
+
+// Creating object
+object threading
+{
+  // Main method
+  def main(args: Array[String])
+  {
+    //generate random ids magasin
+    val rd: Random = new Random
+    var arr: Array[Byte] = new Array[Byte](16)
+    var idsMagasin = new Array[String](1200)
+    var idMagasin =""
+
+    for(i <- 0 until  1200) {
+      // generate random magasin id
+      rd.nextBytes(arr)
+      val sb = new StringBuilder
+      for (b <- arr) {
+        sb.append(String.format("%02x", Byte.box(b)))
+      }
+      idMagasin = sb.toString
+      idMagasin = idMagasin.substring(0, 8) + '-' + idMagasin.substring(8, 12) + '-' + idMagasin.substring(12, 16) + '-' + idMagasin.substring(16, 20) + '-' + idMagasin.substring(20)
+      idsMagasin(i) = idMagasin
+    }
+
+
+    for (k <- 0 until 100)
+    {
+      var th = new MyThread(k, rd, idsMagasin)
+      th.setName(k.toString())
+      th.start()
+    }
+  }
+}
+
